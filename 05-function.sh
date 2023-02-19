b83a0a48abd42f5c9966ccae81bff93eeed0d613
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-19 12:20:59 +0530

    Update learn-shell/

--- 05-function.sh
+++ 05-function.sh
@@ -2,7 +2,14 @@
 
 xyz() {
   echo Hello from Function
+  echo First Arugment - $1
+  echo Second Arugment - $2
+  echo All Arugments - $*
+  echo Number of Arguments - $#
 }
 
+## Main Program
 ## Call a function
-xyz
+xyz 123 456
+
+
