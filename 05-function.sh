e10e9e062c0e4b07d0abf47b34399b5bf388d9cb
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-19 12:30:45 +0530

    Update learn-shell/

--- 05-function.sh
+++ 05-function.sh
@@ -6,10 +6,13 @@ xyz() {
   echo Second Arugment - $2
   echo All Arugments - $*
   echo Number of Arguments - $#
+  echo Value of a = $a
+  b=200
 }
 
 ## Main Program
 ## Call a function
+a=120
 xyz 123 456
-
+echo Value of b = $b
 
