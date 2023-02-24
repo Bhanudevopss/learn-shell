2880dffcacacda8934449d17d69c91d263f7390a
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-24 10:13:16 +0530

    Update learn-shell/

--- 05-function.sh
+++ 05-function.sh
@@ -6,6 +6,7 @@ xyz() {
   echo Second Arugment - $2
   echo All Arugments - $*
   echo Number of Arguments - $#
+  a=300
   echo Value of a = $a
   b=200
 }
@@ -16,3 +17,12 @@ a=120
 xyz 123 456
 echo Value of b = $b
 
+
+abc() {
+  echo Hello
+  return 1
+  echo Bye
+}
+
+abc
+echo Exist Status of abc function = $?
--- 07-read.sh
+++ 07-read.sh
@@ -4,3 +4,4 @@ read -p 'Enter your name: ' name
 
 echo "Your Name = $name"
 
+# Read command will fail for automation because manual intervention is required
